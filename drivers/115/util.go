21326b6e1cb212d046964ba51a0cc51ebc6c9318
lei <142505501+54shen@users.noreply.github.com>
2024-10-03 13:13:00 +0800

    Update util.go

--- drivers/115/util.go
+++ drivers/115/util.go
@@ -20,7 +20,7 @@ import (
 	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 
-	driver115 "github.com/54shen/115driver/pkg/driver"
+	driver115 "github.com/54shen/115driver/tree/main/pkg/driver"
 	crypto "github.com/gaoyb7/115drive-webdav/115"
 	"github.com/orzogc/fake115uploader/cipher"
 	"github.com/pkg/errors"
